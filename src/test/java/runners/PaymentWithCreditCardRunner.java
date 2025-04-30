a8d870ea339d3e0dbd13f2c92ad70f00ffaf0317
rftbtr <rifatbatir@gmail.com>
2025-05-01 00:52:18 +0300

    PaymentWithCreditCard.feature PaymentWithCreditCardRunner.java PaymentWithCreditCardSteps.java Added

---
+++ src/test/java/runners/PaymentWithCreditCardRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.*;
+import utilities.ExtentReportManager;
+import utilities.GWD;
+
+@CucumberOptions(
+        features = {"src/test/java/featureFiles/PaymentWithCreditCard.feature"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
+public class PaymentWithCreditCardRunner extends AbstractTestNGCucumberTests {
+    @BeforeClass
+    @Parameters("browserType")
+    public void setUp(String browserType) {
+        GWD.threadBrowserName.set(browserType);
+        ExtentReportManager.setBrowserName(browserType);
+    }
+
+    @AfterClass
+    public static void writeExtendReport() {
+        ExtentReportManager.writeMetadata();
+    }
+}
