38ca214c2639b61c53a5bbd77074a94953aea7a8
NuriOzturk <nroztrk17@gmail.com>
2025-05-01 17:53:05 +0300

    Created feature and stepDefination classes for Informaiton video

---
+++ src/test/java/stepDefinitions/InformationVideoSteps.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class InformationVideoSteps {
+    @When("The user clicks the E-commerce by E-junkie button")
+    public void theUserClicksTheECommerceByEJunkieButton() {
+    }
+
+    @And("The user clicks the see how is works button")
+    public void theUserClicksTheSeeHowIsWorksButton() {
+    }
+
+    @And("The user clicks the play button")
+    public void theUserClicksThePlayButton() {
+    }
+
+    @Then("The user clicks the close button")
+    public void theUserClicksTheCloseButton() {
+    }
+}
