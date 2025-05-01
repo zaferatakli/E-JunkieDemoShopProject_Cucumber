b47f707f0965ad9e5d398ddbe91cd17cd2591c0a
rftbtr <rifatbatir@gmail.com>
2025-05-01 17:22:30 +0300

    added to the pom.xml file: <dependency>     <groupId>org.seleniumhq.selenium</groupId>     <artifactId>selenium-java</artifactId>     <version>4.31.0</version> </dependency>
    AND
    PaymentWithCreditCardSteps.java
    PaymentWithCreditCard.feature
    files were run, tedtted and added.

--- src/test/java/featureFiles/PaymentWithCreditCard.feature
+++ src/test/java/featureFiles/PaymentWithCreditCard.feature
@@ -2,3 +2,13 @@ Feature: E-Junkie -  Payment Functionality
 
   Scenario: Payment and confirmation by Debit/Credit Card Test
     Given Navigate to E-Junkie homepage
+    And memorize the “Demo eBook” price.
+    And click AddToCart for Demo eBook.
+    And hit the pay by credit card button.
+    When billing details page is displayed, click on the “I am a human” field
+    And enter your payment details.
+    And click the pay button.
+    And the successful completion of the transaction.
+
+
+
--- src/test/java/stepDefinitions/PaymentWithCreditCardSteps.java
+++ src/test/java/stepDefinitions/PaymentWithCreditCardSteps.java
@@ -1,9 +1,65 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import pages.DialogContent;
+import utilities.ConfigReader;
+import utilities.GWD;
 
 public class PaymentWithCreditCardSteps {
     DialogContent dc = new DialogContent();
 
+    @And("memorize the “Demo eBook” price.")
+    public void memorizeTheDemoEBookPrice() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.eBookPrice));
+    }
 
+    @And("click AddToCart for Demo eBook.")
+    public void clickAddToCartForDemoEBook() {
+        dc.demoEBookAddToCart.click();
+    }
+
+    @And("hit the pay by credit card button.")
+    public void hitThePayByCreditCardButton() {
+        GWD.getDriver().switchTo().frame(dc.yourCardIframe);
+        dc.creditCardButton.click();
+    }
+
+    @When("billing details page is displayed, click on the “I am a human” field")
+    public void billingDetailsPageIsDisplayedClickOnTheIAmAHumanField() {
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.billingForm));
+        GWD.getDriver().switchTo().frame(dc.humanIframe);
+        dc.humanField.click();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.captchaFieldCheck));
+
+    }
+
+    @And("enter your payment details.")
+    public void enterYourPaymentDetails() {
+        GWD.getDriver().switchTo().defaultContent();
+        GWD.getDriver().switchTo().frame(dc.yourCardIframe);
+        dc.emailField.sendKeys(ConfigReader.getProperty("email"));
+        dc.confirmEmailField.sendKeys(ConfigReader.getProperty("email"));
+        dc.nameOnCardField.sendKeys(ConfigReader.getProperty("name"));
+
+        GWD.getDriver().switchTo().frame(dc.cardIframe);
+        dc.cardNumberField.sendKeys(ConfigReader.getProperty("validCreditCardNum"));
+        dc.expDateField.sendKeys(ConfigReader.getProperty("validCardExpDate"));
+        dc.cvcField.sendKeys(ConfigReader.getProperty("validCvc"));
+    }
+
+    @And("click the pay button.")
+    public void clickThePayButton() {GWD.getDriver().switchTo().defaultContent();
+        GWD.getDriver().switchTo().frame(dc.yourCardIframe);
+        dc.payButton.click();
+    }
+
+    @And("the successful completion of the transaction.")
+    public void theSuccessfulCompletionOfTheTransaction() {
+        GWD.getDriver().switchTo().defaultContent();
+        GWD.getWait().until(ExpectedConditions.visibilityOf(dc.confirmText));
+        Assert.assertEquals(dc.confirmText.getText(), ConfigReader.getProperty("contactUsSuccessMessage"));
+    }
 }
